6a186f15170aacd09567fcc9d8a97f45bfa840e3
DeveloperDurp <developerdurp@durp.info>
2022-10-11 18:08:35 +0000

    Update readme.MD, vm_templates/cloudinit.sh

--- vm_templates/cloudinit.sh
+++ vm_templates/cloudinit.sh
@@ -2,6 +2,7 @@ export URL="https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream
 export NAME="centos9.qcow2"
 export VM="CentOS9-Template"
 export VMID="99999"
+export LOCATION="domains"
 
 echo Downloading Image
 wget $URL -O $NAME
@@ -10,11 +11,11 @@ echo adding qemu agent
 virt-customize -a $NAME --install qemu-guest-agent
 
 echo setting up VM
-qm create $VMID --name $VM --memory 14336 --cores 6 -cpu host --net0 virtio,bridge=vmbr0
-qm importdisk $VMID $NAME domains
-qm set $VMID --scsihw virtio-scsi-pci --scsi0 domains:vm-$VMID-disk-0,ssd=0 
+qm create $VMID --name $VM --memory 1024 --cores 1 -cpu host --net0 virtio,bridge=vmbr1
+qm importdisk $VMID $NAME $LOCATION
+qm set $VMID --scsihw virtio-scsi-pci --scsi0 $LOCATION:vm-$VMID-disk-0
 qm set $VMID --boot c --bootdisk scsi0 
-qm set $VMID --ide2 domains:cloudinit 
+qm set $VMID --ide2 $LOCATION:cloudinit 
 qm set $VMID --serial0 socket --vga serial0
 qm set $VMID --agent enabled=1
 qm set $VMID --nameserver 192.168.20.1
