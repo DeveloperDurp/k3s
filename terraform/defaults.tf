e98e740b8ec9a02eeaae1db5fc3b3397cf3e6216
DeveloperDurp <developerdurp@durp.info>
2022-11-04 22:03:11 +0000

    Update terraform/variables.tf, terraform/main.tf, terraform/defaults.tf, terraform/terraform.tfvars, .gitlab-ci.yml

---
+++ terraform/defaults.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "http" {}
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "~> 2.9.11"
+    }
+  }
+}
+
+provider "proxmox" {
+    pm_parallel     = 3
+    pm_tls_insecure = true
+    pm_api_url      = var.pm_api_url
+    pm_user         = var.pm_user
+    pm_password     = var.pm_password
+    pm_debug        = false
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,82 @@
+variable "pm_api_url" {
+    description = "API URL to Proxmox provider"
+    type = string
+}
+
+variable "dnsserver" {
+    description = "DNS provider"
+    type = string
+}
+
+variable "sshkeys" {
+    description = "Public SSH key to inject into CloudInit"
+    type = string
+}
+
+variable "pm_password" {
+    description = "Passowrd to Proxmox provider"
+    type = string
+}
+
+variable "pm_user" {
+    description = "UIsername to Proxmox provider"
+    type = string
+    default = "root@pam"
+}
+
+
+variable "k3master" {
+    description = "Defaults of master nodes in K3S"
+    type = object({
+        count    = number
+        name     = list(string)
+        cores    = number
+        memory   = number
+        drive    = string
+        storage  = string
+        template = string
+        node     = string
+        tag      = number
+        ip       = list(number)   
+    })
+    default = {
+        count    = "1"
+        name     = ["master01"]
+        cores    = "2"
+        memory   = "2048"
+        drive    = "20G"
+        storage  = "domains"
+        template = "CentOS9-Template"
+        node     = "overlord"
+        tag      = "20"
+        ip       = ["121"]
+    }
+}
+
+variable "k3server" {
+    description = "Defaults of worker nodes in K3S"
+    type = object({
+        count    = number
+        name     = list(string)
+        cores    = list(number)
+        memory   = list(number)
+        drive    = list(string)
+        storage  = list(string)
+        template = string
+        node     = string
+        tag      = number
+        ip       = list(number)   
+    })
+        default = {
+        count    = "1"
+        name     = ["node01"]
+        cores    = "2"
+        memory   = "4096"
+        drive    = "60G"
+        storage  = "domains"
+        template = "CentOS9-Template"
+        node     = "overlord"
+        tag      = "20"
+        ip       = ["124"]
+    }
+}
