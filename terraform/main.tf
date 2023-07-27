a3219ea48e3cff8b679e2badaa73eee80ce19064
DeveloperDurp <developerdurp@durp.info>
2023-07-27 01:09:49 +0000

    Update 3 files
    - /terraform/main.tf
    - /terraform/terraform.tfvars
    - /ansible/hosts.ini

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,75 +1,29 @@
-resource "proxmox_vm_qemu" "VM" {
-  count       = 1
-  ciuser      = "administrator"
-  vmid        = "201"
-  name        = "TestVM"
-  target_node = "mothership"
-  clone       = "Debian12-Template"
-  qemu_os     = "other"
-  full_clone  = true
-  os_type     = "cloud-init"
-  agent       = 1
-  cores       = 2
-  sockets     = 1
-  cpu         = "host"
-  memory      = 2048
-  scsihw      = "virtio-scsi-pci"
-  bootdisk    = "scsi0"
-  boot        = "c"
-  onboot      = true
-  disk {
-    size    = "20G"
-    type    = "scsi"
-    format  = "raw"
-    storage = "ssd-domains"
-    ssd     = 1
-    backup  = true
-  }
-  network {
-    model  = "virtio"
-    bridge = "vmbr1"
-  }
-  lifecycle {
-    ignore_changes = [
-      network,
-    ]
-  }
-  #Cloud Init Settings
-  ipconfig0    = "ip=192.168.20.20/24,gw=192.168.20.1"
-  searchdomain = "durp.loc"
-  nameserver   = var.dnsserver
-  sshkeys      = var.sshkeys
-}
-
-
-#k3s
-#-----------------------------------------------------
-#
 #resource "proxmox_vm_qemu" "VM" {
-#  count       = var.k3master.count
+#  count       = 1
 #  ciuser      = "administrator"
-#  vmid        = "20${var.k3master.ip[count.index]}"
-#  name        = var.k3master.name[count.index]
-#  target_node = var.k3master.node[count.index]
-#  clone       = var.k3master.template[count.index]
+#  vmid        = "201"
+#  name        = "TestVM"
+#  target_node = "mothership"
+#  clone       = "Debian12-Template"
 #  qemu_os     = "other"
 #  full_clone  = true
 #  os_type     = "cloud-init"
 #  agent       = 1
-#  cores       = var.k3master.cores
+#  cores       = 2
 #  sockets     = 1
 #  cpu         = "host"
-#  memory      = var.k3master.memory
+#  memory      = 2048
 #  scsihw      = "virtio-scsi-pci"
 #  bootdisk    = "scsi0"
 #  boot        = "c"
 #  onboot      = true
 #  disk {
-#    size    = var.k3master.drive
+#    size    = "20G"
 #    type    = "scsi"
-#    storage = var.k3master.storage
+#    format  = "raw"
+#    storage = "ssd-domains"
 #    ssd     = 1
-#    backup  = false
+#    backup  = true
 #  }
 #  network {
 #    model  = "virtio"
@@ -81,8 +35,54 @@ resource "proxmox_vm_qemu" "VM" {
 #    ]
 #  }
 #  #Cloud Init Settings
-#  ipconfig0    = "ip=192.168.20.${var.k3master.ip[count.index]}/24,gw=192.168.20.1"
+#  ipconfig0    = "ip=192.168.20.20/24,gw=192.168.20.1"
 #  searchdomain = "durp.loc"
 #  nameserver   = var.dnsserver
 #  sshkeys      = var.sshkeys
 #}
+#
+
+#k3s
+#-----------------------------------------------------
+
+resource "proxmox_vm_qemu" "VM" {
+  count       = var.k3master.count
+  ciuser      = "administrator"
+  vmid        = "20${var.k3master.ip[count.index]}"
+  name        = var.k3master.name[count.index]
+  target_node = var.k3master.node[count.index]
+  clone       = var.k3master.template[count.index]
+  qemu_os     = "other"
+  full_clone  = true
+  os_type     = "cloud-init"
+  agent       = 1
+  cores       = var.k3master.cores
+  sockets     = 1
+  cpu         = "host"
+  memory      = var.k3master.memory
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
+  boot        = "c"
+  onboot      = true
+  disk {
+    size    = var.k3master.drive
+    type    = "scsi"
+    storage = var.k3master.storage
+    ssd     = 1
+    backup  = false
+  }
+  network {
+    model  = "virtio"
+    bridge = "vmbr1"
+  }
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  #Cloud Init Settings
+  ipconfig0    = "ip=192.168.20.${var.k3master.ip[count.index]}/24,gw=192.168.20.1"
+  searchdomain = "durp.loc"
+  nameserver   = var.dnsserver
+  sshkeys      = var.sshkeys
+}
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,27 +1,27 @@
 dnsserver = "192.168.20.1"
 
-sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTYqag8OKcV6kIitn3Axlyi3Xr9EeybG10wlglw34fYF0pY+OERy7zZKEju4ijZzQ7eWNlcXLYSorm5Tngkvnz4vbM4b9R7gZjTV9drSGDo0BLkMXNuSTrKwGeokcNkxh+HZcWSK4/SE5zPzvkPj1UvmAgQ4P4N79mqPe5/9gAvdrlUWEtuqVdEHc/FMk4kEZsRu4lg58KoghNCRYMYHOyd1rbHsuWpX5NumPxnosWG22jzqj46rUWEXvA7MrCGGbUDlk5+/h7Bvw4O8nGZLEo/qyaYvChTBj/UqYYBssC4VlW/SNJB1yfrklqdtcknmFVJBi174cQtzZDXOerwneh8/+t7wWpcxkWscxYrwdJspzAU/NGk02xDPaG4F1mdgZ6HIZCQAaw/EbaNbiuU+bhdngEIHUvVmdiy4T09FWIWuJxO6FnAiVIU5K8LpqGLTFp7kjOwAczdQ+KVojm/1A5W/ZoTE/y3Ni1fVaOJFCxSgU7qiKAm7hb2ZXvznNgryc="
+sshkeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEjOeWMy3W7GyFQmWO90RXTAGHzOcIkqk4DuTrHn634m"
 
 k3master = {
   count    = 1
-  name     = ["k3s01"]
+  name     = ["master-dev"]
   cores    = 4
   memory   = "4096"
   drive    = "20G"
-  storage  = "NVMeSSD"
-  template = ["CentOS9-Template"]
+  storage  = "ssd-domains"
+  template = ["Debian12-Template"]
   node     = ["overlord"]
-  ip       = ["121"]
+  ip       = ["10"]
 }
 
 k3server = {
-  count    = 4
-  name     = ["node01", "node02","node03","node04"]
+  count    = 1
+  name     = ["node01-dev"]
   cores    = 4
-  memory   = "8192"
-  drive    = ["60G","60G","60G","60G"]
-  storage  = "NVMeSSD"
-  template = ["CentOS9-Template","CentOS9-Template","CentOS9-Template","CentOS9-Template"]
-  node     = ["overlord","overlord","overlord","overlord"]
-  ip       = ["141", "142", "143", "144"]
+  memory   = "4096"
+  drive    = ["60G"]
+  storage  = "ssd-domains"
+  template = ["Debian12-Template"]
+  node     = ["mothership"]
+  ip       = ["20"]
 }
