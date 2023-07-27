16ed6feca38a14fba7551b01461f144c560ee4a8
DeveloperDurp <developerdurp@durp.info>
2023-07-27 01:17:28 +0000

    Update file main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,7 +45,7 @@
 #k3s
 #-----------------------------------------------------
 
-resource "proxmox_vm_qemu" "VM" {
+resource "proxmox_vm_qemu" "k3master" {
   count       = var.k3master.count
   ciuser      = "administrator"
   vmid        = "20${var.k3master.ip[count.index]}"
@@ -67,6 +67,7 @@ resource "proxmox_vm_qemu" "VM" {
   disk {
     size    = var.k3master.drive
     type    = "scsi"
+    format  = "raw"
     storage = var.k3master.storage
     ssd     = 1
     backup  = false
@@ -82,6 +83,49 @@ resource "proxmox_vm_qemu" "VM" {
   }
   #Cloud Init Settings
   ipconfig0    = "ip=192.168.20.${var.k3master.ip[count.index]}/24,gw=192.168.20.1"
+  searchdomain = "durp.loc"
+  nameserver   = var.dnsserver
+  sshkeys      = var.sshkeys
+}
+
+resource "proxmox_vm_qemu" "k3server" {
+  count       = var.k3server.count
+  ciuser      = "administrator"
+  vmid        = "20${var.k3server.ip[count.index]}"
+  name        = var.k3server.name[count.index]
+  target_node = var.k3server.node[count.index]
+  clone       = var.k3server.template[count.index]
+  qemu_os     = "other"
+  full_clone  = true
+  os_type     = "cloud-init"
+  agent       = 1
+  cores       = var.k3server.cores
+  sockets     = 1
+  cpu         = "host"
+  memory      = var.k3server.memory
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
+  boot        = "c"
+  onboot      = true
+  disk {
+    size    = var.k3server.drive
+    type    = "scsi"
+    format  = "raw"
+    storage = var.k3server.storage
+    ssd     = 1
+    backup  = false
+  }
+  network {
+    model  = "virtio"
+    bridge = "vmbr1"
+  }
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  #Cloud Init Settings
+  ipconfig0    = "ip=192.168.20.${var.k3server.ip[count.index]}/24,gw=192.168.20.1"
   searchdomain = "durp.loc"
   nameserver   = var.dnsserver
   sshkeys      = var.sshkeys
