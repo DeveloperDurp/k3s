855ad01b3ef350b90231bda03d18e38613f3f910
DeveloperDurp <developerdurp@durp.info>
2023-12-13 13:05:45 +0000

    Update file cloudinit.sh

--- vm_templates/cloudinit.sh
+++ vm_templates/cloudinit.sh
@@ -2,25 +2,23 @@ export URL="https://cloud.debian.org/images/cloud/bookworm/20230711-1438/debian-
 export NAME="Debian12.qcow2"
 export VM="Debian12-Template"
 export VMID="99999"
-export LOCATION="ssd-domains"
+export LOCATION="cache-domains"
 
 echo Downloading Image
 wget $URL -O $NAME
 
 echo adding qemu agent
-virt-customize -a $NAME --install qemu-guest-agent
+virt-customize -a $NAME --install qemu-guest-agent,docker,ansible
 
 echo setting up VM
-qm create $VMID --name $VM --memory 1024 --cores 1 -cpu host --net0 virtio,bridge=vmbr1
+qm create $VMID --name $VM --memory 1024 --cores 1 -cpu host --net0 virtio,bridge=vmbr0
 qm importdisk $VMID $NAME $LOCATION
 qm set $VMID --scsihw virtio-scsi-pci --scsi0 $LOCATION:$VMID/vm-$VMID-disk-0.raw,size=10G
 qm set $VMID --boot c --bootdisk scsi0 
 qm set $VMID --ide2 $LOCATION:cloudinit 
 qm set $VMID --serial0 socket --vga serial0
 qm set $VMID --agent enabled=1
-qm set $VMID --nameserver 192.168.20.1
-qm set $VMID --searchdomain durp.loc
-qm set $VMID --ciuser administrator
+qm set $VMID --ipconfig0 ip=dhcp
 
 echo Converting to Template
 qm template $VMID
